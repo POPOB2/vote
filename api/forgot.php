380c2fb62ecc624225a81d64fc0e41d4ba8fa360
PB2 <nft192891@gmail.com>
2022-07-03 20:16:43 +0800

    找回密碼的判斷

---
+++ api/forgot.php
@@ -0,0 +1,17 @@
+<?php
+include_once "base.php";
+
+$acc=$_POST['acc'];
+
+$sql="SELECT * FROM `users` WHERE `acc`='$acc'";
+
+$user=$pdo->query($sql)->fetch();
+
+if(!empty($user)){
+    $chkok="當初設置的密碼提示為:<br>".$user['passnote'];
+    header("location:../forgot.php?error=$chkok");
+}else{
+    $error="查無此帳號";
+    header("location:../forgot.php?error=$error");
+}
+?>
