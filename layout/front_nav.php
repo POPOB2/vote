b21b834a02ea8539514d2fdcf0fb2c53e0888de6
PB2 <nft192891@gmail.com>
2022-07-03 20:18:51 +0800

    新增登入登出SESSION的判斷

--- layout/front_nav.php
+++ layout/front_nav.php
@@ -1,10 +1,27 @@
 
 
 <!-- 前台用的按紐 -->
-
+<?php
+include_once "./api/base.php";
+$SES=session_destroy();
+?>
 <nav>
         <a href="index.php">回首頁</a>
-        <a href="index.php">登入</a>
+
+        <?php
+        if(isset($_SESSION['user'])){
+        ?>
+        <a href="login.php" onclick="<?=$SES;?>">登出</a>
+        <?php
+        }else{
+        ?>
+        <a href="login.php">登入</a>
+        <?php
+        }
+        ?>
+
         <a href="index.php">投票列表</a>
         <a href="index.php">投票結果</a>
+    </nav>
+        
 </nav>
