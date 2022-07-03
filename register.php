72463dc93a725dfbe597b0a72104ca7395fc47ab
PB2 <nft192891@gmail.com>
2022-07-03 20:20:52 +0800

    註冊頁面

--- register.php
+++ register.php
@@ -9,43 +9,39 @@
     <title>註冊會員</title>
 </head>
 <body>
-
-    <h1>註冊會員</h1>
+<div class="main">
+    <p class="top">註冊會員</p>
     <form action="./api/register.php" method="post">
         <table>
             <tr>
-                <td>帳號</td>
-                <td><input type="text" name="acc" id=""></td>
+                <td><input type="text" name="acc" placeholder="帳號"></td>
             </tr>
             <tr>
-                <td>密碼</td>
-                <td><input type="password" name="pw" id=""></td>
+                <td><input type="password" name="pw" placeholder="密碼"></td>
             </tr>
             <tr>
-                <td>名稱</td>
-                <td><input type="text" name="name" id=""></td>
+                <td><input type="text" name="name" placeholder="名稱"></td>
             </tr>
             <tr>
-                <td>生日</td>
-                <td><input type="date" name="birthday" id=""></td>
+                <td><input type="date" name="birthday"></td>
             </tr>
             <tr>
-                <td>住址</td>
-                <td><input type="text" name="addr" id=""></td>
+                <td><input type="text" name="addr" placeholder="住址"></td>
             </tr>
             <tr>
-                <td>信箱</td>
-                <td><input type="email" name="email" id=""></td>
+                <td><input type="email" name="email" placeholder="信箱"></td>
             </tr>
             <tr>
-                <td>密碼提示</td>
-                <td><input type="text" name="passnote" id=""></td>
+                <td><input type="text" name="passnote" placeholder="密碼提示"></td>
             </tr>
         </table>
         <div>
-            <input type="submit" value="註冊">
-            <input type="reset" value="重置">
+            <input class="register" type="submit" value="註冊">
+            <input class="reset" type="reset" value="重置">
+            <a class="toback" href="./login.php">返回</a>
         </div>
     </form>
+    
+</div>
 </body>
 </html>
