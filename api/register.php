6e941917b238ebf43e09d6b24646f8929d48705f
PB2 <nft192891@gmail.com>
2022-07-03 20:17:12 +0800

    註冊頁的判斷

--- api/register.php
+++ api/register.php
@@ -1,26 +1,18 @@
 
-
-<!-- 用於判斷註冊 不需要html頁面 -->
-
 <?php
 include_once "base.php";
-
-
-$acc=$_POST['acc'];
-
-
-$pw=md5($_POST['pw']);
-                        
-
+                    
+$remind='';
 $sql=[
     'acc'=>$_POST['acc'],
-    'pw'=>$_POST['pw'],
+    'pw'=>md5($_POST['pw']),
     'name'=>$_POST['name'],
     'birthday'=>$_POST['birthday'],
     'addr'=>$_POST['addr'],
     'email'=>$_POST['email'],
     'passnote'=>$_POST['passnote']
 ];
+
 
 save('users',$sql);
 header("location:../login.php");
