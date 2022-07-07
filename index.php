41080cfd5ce6f27ec92425ca638407d218f93d9a
PB2 <nft192891@gmail.com>
2022-07-07 21:20:58 +0800

    調整首頁排版

--- index.php
+++ index.php
@@ -8,28 +8,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>安安線上投票中心</title>
     <link rel="stylesheet" href="./css/basic.css">
-    <link rel="stylesheet" href="./css/front.css"> <!-- link後面會蓋掉前面的 -->
+    <link rel="stylesheet" href="./css/front.css">
+    <!-- <link rel="stylesheet" href="./css/index.css"> -->
 </head>
 <body>
     <!-- 前台 -->
     <div id="header">
-        <?php include "./layout/header.php";
-              include "./layout/front_nav.php"
-        ?>
+        <?php include "./layout/header.php";?>
     </div>
-    <div id='container'>
-        <?php
-        if(isset($_GET['do'])){
-            $file='./front/'.$_GET['do'].".php";
-        }
-        if(isset($file)&& file_exists($file)){
-            include "$file";
-        }else{
-            include "./front/vote_list.php";
-        }
-        ?>
+
+
+    
+
+    <div class="main">
+    
+        <div id='container'>
+            
+            <?php
+            if(isset($_GET['do'])){
+                $file='./front/'.$_GET['do'].".php";
+            }
+            if(isset($file)&& file_exists($file)){
+                include "$file";
+            }else{
+                include "./front/vote_list.php";
+            }
+            ?>
+        </div>
+        <aside>
+        <?php include "./layout/front_nav.php"; ?>
+        </aside>
     </div>
-    </div>
+
+
     <div>
         <?php include "./layout/footer.php";?>
     </div>
