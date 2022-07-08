500c449dee26ac6381885a143f6f7584fb3b6f7d
PB2 <nft192891@gmail.com>
2022-07-08 23:04:15 +0800

    後台選單列表

--- layout/back_nav.php
+++ layout/back_nav.php
@@ -1,9 +1,25 @@
+<?php
+include_once "./api/base.php";
+?>
 
+<!-- 後台用的按鈕 -->
 
-<!-- 前台用的按鈕 -->
-
-<nav>
+<nav>   
+        <a href="index.php">回首頁</a>
+        
         <a href="back.php">投票列表</a>
         <a href="?do=add_vote">新增投票</a>
-        <a href="?do=admin_type">分類管理</a>
+        <a href="?do=admin_type">新增分類</a>
+        <a href="member_center.php">帳號管理</a>
+        <?php
+        if(isset($_SESSION['user'])){
+        ?>
+        <a href="./api/logout.php">登出</a>
+        <?php
+        }else{
+        ?>
+        <a href="login.php">登入</a>
+        <?php
+        }
+        ?>
 </nav>
