bd8d07e8b7045ea5ec293422615012c89b7275a5
PB2 <nft192891@gmail.com>
2022-07-08 23:03:34 +0800

    前台選單列表

--- layout/front_nav.php
+++ layout/front_nav.php
@@ -3,25 +3,23 @@
 <!-- 前台用的按紐 -->
 <?php
 include_once "./api/base.php";
-$SES=session_destroy();
 ?>
 <nav>
-        <a href="index.php">回首頁</a>
+    <a href="index.php">回首頁</a>
+    <!-- <a href="index.php">投票列表</a> -->
+    <!-- <a href="index.php">投票結果</a> -->
+    <a href="member_center.php">管理帳戶</a>
+    <a href="back.php">管理投票項目</a>
 
-        <?php
-        if(isset($_SESSION['user'])){
-        ?>
-        <a href="login.php" onclick="<?=$SES;?>">登出</a>
-        <?php
-        }else{
-        ?>
-        <a href="login.php">登入</a>
-        <?php
-        }
-        ?>
-
-        <a href="index.php">投票列表</a>
-        <a href="index.php">投票結果</a>
-    </nav>
-        
+    <?php
+    if(isset($_SESSION['user'])){
+    ?>
+    <a href="./api/logout.php">登出</a>
+    <?php
+    }else{
+    ?>
+    <a href="login.php">登入</a>
+    <?php
+    }
+    ?>
 </nav>
