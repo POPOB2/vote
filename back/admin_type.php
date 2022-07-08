e334f87c170357b74e46c06779149d44e7c404e2
PB2 <nft192891@gmail.com>
2022-07-08 23:11:33 +0800

    後台/選單/新增分類/內頁

--- back/admin_type.php
+++ back/admin_type.php
@@ -3,11 +3,9 @@
 <!-- 分類管理 -->
 <!-- 以POST傳資料到api/add_type.php, 資料內容標記為name(該標記用於api/add_type.php內的base.php引用的saveFunction辨識放到表的哪個欄)   -->
 <form action="./api/add_type.php" method="post">
-    <div>
+    <div class="class_name">
         <label for="name">分類名稱</label>
         <input type="text" name="name" id="name">
-    </div>
-    <div>
         <input class="btn btn-primary" type="submit" value="送出">
     </div>
 </form>
