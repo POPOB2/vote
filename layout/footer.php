6b6335d2dda2fb4a6a9f3367b9febdac51038e2e
PB2 <nft192891@gmail.com>
2022-07-10 18:11:52 +0800

    更改上幅與下幅的內容

--- layout/footer.php
+++ layout/footer.php
@@ -3,5 +3,5 @@
 
 
 <footer>
-    <div class="right">安安投票中心版權所有</div>
+    <div class="connection">嗚嘎嘎投票中心版權所有</div>
 </footer>
--- layout/header.php
+++ layout/header.php
@@ -1,8 +1,5 @@
 
-
-
-
-
-<div class="slider" style="background-image:url(./upload/B.jpg)">
-    
+<div class="slider">
+    <div></div>
+    <p class="p1">歡迎來到安安投票系統</p>
 </div>
