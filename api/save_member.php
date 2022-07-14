220cf5997d30013d974e0d88449fbcd25dc7d2e3
PB2 <nft192891@gmail.com>
2022-07-14 16:34:46 +0800

    更改查找資料的方式 更正220上的錯誤

--- api/save_member.php
+++ api/save_member.php
@@ -3,30 +3,33 @@
 
 <?php
 include_once "base.php";
-$pw=md5($_POST['pw']);
+$users=find('users',['id'=>$_GET['id']]);
+$pw=($_POST['pw']);
 // 資料更新users資料表 SET設定pw 使用的值為{$_POST['pw']}以此類推 WHERE如何定位哪一筆資料進行改變 `id`='{$_POST['id']}'
-$sql="UPDATE `users`
-        SET `pw`='$pw',
-            `name`='{$_POST['name']}',
-            `birthday`='{$_POST['birthday']}',
-            `addr`='{$_POST['addr']}',
-            `email`='{$_POST['email']}',
-            `passnote`='{$_POST['passnote']}'
-      WHERE `id`='{$_POST['id']}'";
+$users['pw']=$pw;
+$users['name']=$_POST['name'];
+$users['birthday']=$_POST['birthday'];
+$users['addr']=$_POST['addr'];
+$users['email']=$_POST['email'];
+$users['passnote']=$_POST['passnote'];
+
 
 
 function alert($text){
       echo "<script>alert('$text');window.history.back(-1)</script>";
 }
+// 密碼先設空值若密碼有值 才改變 否則不改
 
-
-if(!empty($pw&&$_POST['name']&&$_POST['birthday']&&$_POST['addr']&&$_POST['email']&&$_POST['passnote'])){
-      $pdo->exec($sql); // $pdo 執行儲存內容($sql) // 把表單資料存進資料庫
+if(!empty($_POST['name']&&$_POST['birthday']&&$_POST['addr']&&$_POST['email']&&$_POST['passnote'])){
+      // $pdo->exec($sql); // $pdo 執行儲存內容($sql) // 把表單資料存進資料庫
+      save('users',$users);
       header("location:../member_center.php");
 }else{
       $text2="編輯的個人資訊有誤,請更改後再送出";
       alert($text2);
 }
+
+
 
 
 
