d2dc0ef246b62f8b838f7d6b739191f4f7ff97a9
PB2 <nft192891@gmail.com>
2022-07-14 16:34:15 +0800

    更改查找資料的方式 更正220上的錯誤

--- api/forgot.php
+++ api/forgot.php
@@ -3,9 +3,10 @@ include_once "base.php";
 
 $acc=$_POST['acc'];
 
-$sql="SELECT * FROM `users` WHERE `acc`='$acc'";
+// $sql="SELECT * FROM `users` WHERE `acc`='$acc'";
+// $user=$pdo->query($sql)->fetch();
 
-$user=$pdo->query($sql)->fetch();
+$user=find('users',['acc'=>$_POST['acc']]);
 
 if(!empty($user)){
     $chkok="當初設置的密碼提示為:<br>".$user['passnote'];
